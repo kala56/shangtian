e696faae5aa920b07ecfa2c6207ea28e2c821951
kala56 <2220426075@qq.com>
2017-07-21 19:27:04 +0800

    Signed-off-by: kala56 <2220426075@qq.com>

--- js/skeySkype.js
+++ js/skeySkype.js
@@ -7,30 +7,39 @@ app.controller("skeyController",function($scope,$http){
 		$scope.skeyList = data.data.data.list;
 		console.log($scope.skeyList);
 	});
-	
 	$http({
+
 		url:"data/shangtian.php?type=setList&occasion_id=30&pageIndex=1"
 	}).then(function(data){
 		console.log(data);
 		$scope.skeyProduct = data.data.data.list;
 		console.log($scope.skeyProduct);
 	});
-	
 	$http({
 		url:"data/shangtian.php?type=setList&occasion_id=19&pageIndex=1"
 	}).then(function(data){
 		console.log(data);
-		$scope.lifeProduct = data.data.data.list;
-		console.log($scope.lifeProduct);
+		$scope.liveProduct = data.data.data.list;
+		console.log($scope.liveProduct);
+	
 	});
-	console.log($(".title li a").length);
-	for(var i = 0;i< $(".title li a").length;i++){
-		$($(".title li a")[i]).click(function(){
-			$scope.index = $(".title li a").index(this);
-			console.log("-=-=-=");
-		});
-	}
+	$http({
+		url:"data/shangtian.php?type=setList&occasion_id=22&pageIndex=1"
+	}).then(function(data){
+		console.log(data);
+		$scope.schoolProduct = data.data.data.list;
+		console.log($scope.schoolProduct);
+	
+	});
+	$http({
+		url:"data/shangtian.php?type=setList&occasion_id=23&pageIndex=1"
+	}).then(function(data){
+		console.log(data);
+		$scope.trystProduct = data.data.data.list;
+		console.log($scope.trystProduct);
+	
+	});
+	
 });
-// $($(".title>li>a")[0]).click(function () {
-// 	$($(".title>li>a")[0]).attr("href","#best");
-// })
+
+
