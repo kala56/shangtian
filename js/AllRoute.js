05ca4ad689ec65443048ea7e210663b57985a029
Xingen123 <335728491@qq.com>
2017-07-21 20:49:29 +0800

    Signed-off-by: Xingen123 <335728491@qq.com>

--- js/AllRoute.js
+++ js/AllRoute.js
@@ -1,27 +1,43 @@
-var app = angular.module("app",["ngRoute"]);
+var app = angular.module("app",["ngRoute","me-lazyload"]);
 // config配置路由
 app.config(['$routeProvider',function($routeProvider){
 	$routeProvider.when("/home",{  //主页
 		templateUrl:"",
 		controller:""
 	})
-	.when("/piece",{  //一套
-		templateUrl:"html/titleList.html",
-		controller:"skeyController"
-	})
-	.when("/cover",{  //一件
-		templateUrl:"",
-		controller:""
-	})
+	// .when("/piece",{  //一套
+	// 	templateUrl:"",
+	// 	controller:""
+	// })
+	// .when("/cover",{  //一件
+	// 	templateUrl:"",
+	// 	controller:""
+	// })
 	.when("/article",{  //一篇
 		templateUrl:"html/article.html",
 		controller:"article_con"
 	})
 	.when("/aside",{  //侧边栏
-		templateUrl:"",
+		templateUrl:"html/aside.html",
 		controller:""
 	})
-	.otherwise({
-		redirectTo:'/home' //默认
-	})
+	// .when("/myself",{  //个人中心
+	// 	templateUrl:"html/myself.html",
+	// 	controller:"myself_con"
+	// })
+	// .otherwise({
+	// 	redirectTo:'/home' //默认
+	// })
 }]);
+
+app.controller("rootController",function($scope){
+	$scope.isShow = true;
+	$scope.showImg = function(){
+		// console.log("------"+$scope.isShow)
+		$scope.isShow = !$scope.isShow;
+	}
+	$scope.hideImg = function(){
+		console.log("------"+$scope.isShow)
+		$scope.isShow = !$scope.isShow;
+	}
+})
